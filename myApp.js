a32fa47714962264ff497c0704616c78ea0ef1fb
Stuart Campbell <56056589+stueyc@users.noreply.github.com>
2021-01-19 15:34:33 +1100

    serving json and public files

--- myApp.js
+++ myApp.js
@@ -2,13 +2,13 @@ var express = require('express');
 var app = express();
 
 console.log("Hello World")
-
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 })
 
-
+app.get('/json', (req, res) => res.json({"message": "Hello json"}))
 
 
 
