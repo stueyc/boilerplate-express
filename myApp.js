42b5ff3dfff1e352819877717d6a03348beec972
Stuart Campbell <56056589+stueyc@users.noreply.github.com>
2021-01-19 18:04:16 +1100

    finished 1 .env and 2 middleware lessons

--- myApp.js
+++ myApp.js
@@ -2,16 +2,34 @@ var express = require('express');
 var app = express();
 
 console.log("Hello World")
+
 app.use(express.static(__dirname + '/public'))
+
+app.use((req, res, next) => {
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip)
+  next()
+})
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 })
 
-app.get('/json', (req, res) => res.json({"message": "Hello json"}))
+app.get('/json', (req, res) => {
+  let message = 'Hello json'
+  if (process.env.MESSAGE_STYLE == 'uppercase'){
+    message = message.toUpperCase()
+  }
+  res.json({"message": message})
+})
 
 
-
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString()
+  next()
+},
+(req, res) => {
+  res.json({"time": req.time})
+})
 
 
 
